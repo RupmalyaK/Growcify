9190df4b15c80d3129f3aa6879d017da9f5a4e4e
Rupmalya Kumar <rupzonthemove@gmail.com>
2021-01-15 18:45:49 +0700

    Updates

---
+++ precache-manifest.688b1073cf1c8952c2b71cbcf6ab8f11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d5ea904d4d1e09bc372570f79be3cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b0aae3b7440c031c960",
+    "url": "/static/css/4.f2fb09fa.chunk.css"
+  },
+  {
+    "revision": "214a573298a67e2bd475",
+    "url": "/static/css/main.f183de13.chunk.css"
+  },
+  {
+    "revision": "d3da8f64cd1661b7f034",
+    "url": "/static/js/2.97e7dd64.chunk.js"
+  },
+  {
+    "revision": "aa6e64f3917ba13c63e8",
+    "url": "/static/js/3.3d14a2be.chunk.js"
+  },
+  {
+    "revision": "5b0aae3b7440c031c960",
+    "url": "/static/js/4.e2771e94.chunk.js"
+  },
+  {
+    "revision": "214a573298a67e2bd475",
+    "url": "/static/js/main.772864f8.chunk.js"
+  },
+  {
+    "revision": "925598d7142ba747c4d2",
+    "url": "/static/js/runtime-main.71754e9e.js"
+  }
+]);
